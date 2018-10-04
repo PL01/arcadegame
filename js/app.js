6ce5ee4da4de3ea6b45997084e2919d62ea3fb19
Pablo Lara <pablolara01@yahoo.com>
2018-10-03 20:36:05 -0400

    Adding in Enemy.

--- js/app.js
+++ js/app.js
@@ -1,48 +1,29 @@
-// Hero class
-    // Constructor
-        // Properties
-            // x position
-            // y position
-            // Sprite image
-        // Methods
-            // Update position
-                // Check collision here
-                    // Did player x and y collide with enemy?
-                // Check win here?
-                    // Did player x and y reach final tile?
-            // Render
-                // Draw player sprite on current x and y coordinate position.
-            // Handle keyboard input
-                // Update player's x and y property according to input
-            // Reset Hero
-                // Set x and y to starting x and y position
-
 class Hero {
-    constructor(){
-        this.sprite = 'images/char-boy.png'; 
-        // property value is set to the path/link for the char-boy image
-        
-        this.step = 101; 
+    constructor() {
+        this.sprite = 'images/char-boy.png';
+        // The image/sprite for our hero, this uses a helper we've provided to easily load images
+
+        this.step = 101;
         // step property will be the distance from one block to another along the x-axis (101 is the width of each block)
-        
-        this.jump = 83; 
+
+        this.jump = 83;
         // block property will be the distance from one block to another along the y-axis (83 is the height of each block)
-        
-        this.startX = this.step * 2; 
+
+        this.startX = this.step * 2;
         // this property places the hero at the middle block on the x-axis
-        
-        this.startY = (this.jump * 5) - 20; 
+
+        this.startY = (this.jump * 5) - 20;
         // this property places the hero 5 blocks down from the top row, we subtract by 20 to fix its position.
-        
-        this.x = this.startX; 
+
+        this.x = this.startX;
         //this property references the starting position on the x-axis
-        
-        this.y = this.startY; 
+
+        this.y = this.startY;
         //this property references the starting position on the y-axis
     }
 
     // Render method: Draw hero sprite on current x and y coord position
-    render(){
+    render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
     /*
@@ -50,11 +31,11 @@ class Hero {
         The first argument is a get method of the Resources object, this'll return a cached image of our sprite.
         The last two arguments call for the current property values of the x & y coord.
     */
-    handleInput(input){
-        switch(input){ 
-        //We check the value of the player's input and match it to the correct direction
+    handleInput(input) {
+        switch (input) {
+            //We check the value of the player's input and match it to the correct direction
             case 'left':
-                if(this.x > 0) {
+                if (this.x > 0) {
                     this.x -= this.step;
                 }
                 // Checks if the hero's x property is greater than 0 (left side of the canvas)
@@ -80,7 +61,7 @@ class Hero {
                 */
                 break;
             case 'down':
-                if (this.y < this.jump * 4){
+                if (this.y < this.jump * 4) {
                     this.y += this.jump;
                 }
                 /*Similar to the how we did with the right side.
@@ -93,20 +74,12 @@ class Hero {
     }
 }
 
-//player is a constant variable, set to a new object.
-const player = new Hero();
-
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // x position (I added this in)
-    // y position (I added this in)
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.x = 0; 
+    this.y = 0;
+    this.sprite = 'images/enemy-bug.png'; 
+    this.step = 101;
 };
 
 // Update the enemy's position, required method for game
@@ -116,12 +89,16 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
 
-    // I added this in
     // If enemy is not passed boundary
-    // Move forward
-    // Increment x by speed * dt
-    // Else
-    // Reset position to start
+    if(this.x < this.step * 4) {
+        // Move forward
+        // Increment x by speed * dt.
+        this.x += 20 * dt;
+    }
+    else{
+        // Reset position to start
+        this.x = 0;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -132,13 +109,14 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+const player = new Hero() // player is a constant variable, set to a new object called Hero.
+const bug1 = new Enemy(); // bug1 is a constant variable, set to a new object called Enemy.
+const allEnemies = []; // allEnemies is an array that stores all our enemies
+allEnemies.push(bug1); // push bug1 into the array, allEnemies.
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
